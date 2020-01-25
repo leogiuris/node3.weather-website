840d0e3560749dc434a9f46f6210b9fbc3877608
leogiuris <leogiuris@gmail.com>
2020-01-25 00:43:44 -0300

    altered forecast message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/e6070147a33d63186e1cc73ffa7a6146/' + latitude + ',' + longitude + '?units=si'
 
-    request({url, json: true}, (error, response) => {
+    request({url, json: true}, (error, {body}) => {
         if (error) {
             callback('connection error', undefined)
-        } else if(response.body.error) {
+        } else if(body.error) {
             callback('Unable to get forecast.', undefined)
         } else {
-            callback(undefined, response.body.currently.summary + ' and it\'s ' + response.body.currently.temperature + ' degrees')
+            callback(undefined, body.daily.data[0].summary + ' It\'s ' + body.currently.temperature + 'º with ' + body.daily.data[0].precipProbability + '% chance of rain.')
         }
     })
 }
