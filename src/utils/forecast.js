58011f1e5da2753adc3a536d09727e41d6d4ee46
leogiuris <leogiuris@gmail.com>
2020-01-25 00:57:45 -0300

    more forecast modifications

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error) {
             callback('Unable to get forecast.', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It\'s ' + body.currently.temperature + 'º with ' + body.daily.data[0].precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + 'ºC with a ' + (body.currently.precipProbability * 100) + '% chance of rain.' +' Today\'s high is '+ body.daily.data[0].temperatureHigh +'ºC with a low of ' + body.daily.data[0].temperatureLow + 'ºC.')
         }
     })
 }
